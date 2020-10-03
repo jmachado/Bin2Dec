008dc37730ca7ef6164e38e824ed829e9ce681d9
Jose MACHADO <jmachado-out@redoute.pt>
2020-10-03 20:34:10 +0100

    adds some logic just to see if github pages works well

--- logic.js
+++ logic.js
@@ -1,2 +1,14 @@
+function changeBackgroundColor(){
+    setInterval(() => {
+        let body = document.getElementById("cenas");
+        if(body.style.backgroundColor == "yellow"){
+            body.style.backgroundColor = "green";
+        } else {
+            body.style.backgroundColor = "yellow";
+        }
+    }, 1000);
+    
+}
 
+changeBackgroundColor();
 
